579f554dd33d5576ceb2216e6c01636f30f3b906
plato <elephamaximu@gmail.com>
2021-12-25 11:58:49 +0900

    make custom event emitter with function constructor

---
+++ app.js
@@ -0,0 +1,15 @@
+// make custom event emitter with function constructor
+const Emitter = require('./emitter');
+
+const emtr = new Emitter();
+
+emtr.on('greet', function () {
+	console.log('someone said hello');
+});
+
+emtr.on('greet', function () {
+	console.log('greeting occurred');
+});
+
+console.log('Hello');
+emtr.emit();
