55306e49496583e838eab760ef71f65355957721
plato <elephamaximu@gmail.com>
2021-12-26 12:44:02 +0900

    callbacks

--- app.mjs
+++ app.mjs
@@ -1,18 +1,13 @@
-// Typed Arrays
+// callbacks
 
-// 8bytes = 64bits
-// This is not coming from Node
-// coming from v8 JavaScript engine
-const buffer = new ArrayBuffer(8);
+function greet(callback) {
+	const data = {
+		name: 'plato',
+		age: 35,
+	};
 
-// view is actually Typed Array meaning that it's an array
-// I can deal with it as an array.
-// and then give it to buffer to work with.
-// simply a way for me to deal with the binary data in the buffer.
-// we can store two numbers in this view 64/32
-var view = new Int32Array(buffer);
+	callback(data);
+}
 
-view[0] = 5;
-view[1] = 15;
-
-console.log(view);
+greet((data) => console.log(`name: ${data.name}`));
+greet((data) => console.log(`age: ${data.age}`));
