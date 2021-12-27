760dc14af8d02fc76c9c8dbbf58d206dd59901a8
plato <elephamaximu@gmail.com>
2021-12-28 00:39:13 +0900

    Copy file by piping

--- app.mjs
+++ app.mjs
@@ -1,19 +1,12 @@
-// Copy file by listening for the data event
+// Copy file by piping
 
 import fs from 'fs';
 import path from 'path';
 
 const __dirname = path.resolve();
 
-const readable = fs.createReadStream(__dirname + '/greet.txt', {
-	encoding: 'utf8',
-	highWaterMark: 2 * 1024,
-});
+const readable = fs.createReadStream(__dirname + '/greet.txt');
 
-const writable = fs.createWriteStream(__dirname + '/greetcopy.txt', {
-	encoding: 'utf8',
-});
+const writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-readable.on('data', function (chunk) {
-	writable.write(chunk);
-});
+readable.pipe(writable);
