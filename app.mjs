df9efac588bc9f2c19c92e0b5c2c896d8039f3d6
plato <elephamaximu@gmail.com>
2021-12-27 10:12:42 +0900

    Files and fs asynchronous

--- app.mjs
+++ app.mjs
@@ -1,21 +1,17 @@
-// Files and fs synchronous
+// Files and fs asynchronous
 
-// wraps up the C++ code that lets me get lower level
-// and deal directly with files.
 import fs from 'fs';
 import path from 'path';
 
-// I can then go get some data from the text file.
-// Now we're dealing with files on our operating system
-// that means we're dealing with, normally, binary data
-// So I need to be able to deal with binary data in reading the file.
-
 const __dirname = path.resolve();
-const greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
 
-// It's looking at the file, it will read the binary data,
-// and then it will use the encoding to determine what those zeros and ones mean,
-// What they ultimately map to as characters.
-// When my file is read, it loads the contents of the file into the buffer first
-// and then uses the encoding so I can get the string back.
-console.log(greet);
+// pattern : error first callback
+const greet = fs.readFile(
+	__dirname + '/greet.txt',
+	'utf8',
+	function (err, data) {
+		console.log(data);
+	}
+);
+
+console.log('done!');
