6a7849a50343abd8af41d70389ea2d0ca7d022f0
plato <elephamaximu@gmail.com>
2021-12-28 00:03:57 +0900

    Copy file by listening for the data event

--- app.mjs
+++ app.mjs
@@ -1,19 +1,19 @@
-// Streams
+// Copy file by listening for the data event
 
 import fs from 'fs';
 import path from 'path';
 
 const __dirname = path.resolve();
 
-// I want to have this give me that ReadStream object
 const readable = fs.createReadStream(__dirname + '/greet.txt', {
 	encoding: 'utf8',
 	highWaterMark: 2 * 1024,
 });
 
-// It has EventEmitter down the pipeline, I can listen to an event
-// an event that's emitted by the ReadStream is 'data'
+const writable = fs.createWriteStream(__dirname + '/greetcopy.txt', {
+	encoding: 'utf8',
+});
+
 readable.on('data', function (chunk) {
-	console.log(chunk);
-	console.log(chunk.length);
+	writable.write(chunk);
 });
