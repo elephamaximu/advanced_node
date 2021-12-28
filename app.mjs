5be8c7295f4fca9df15412946d41ce9bd85c4618
plato <elephamaximu@gmail.com>
2021-12-28 17:55:06 +0900

    pipe method chaining

--- app.mjs
+++ app.mjs
@@ -1,7 +1,8 @@
-// Copy file by piping
+// pipe method chaining
 
 import fs from 'fs';
 import path from 'path';
+import { createGzip } from 'zlib';
 
 const __dirname = path.resolve();
 
@@ -9,4 +10,12 @@ const readable = fs.createReadStream(__dirname + '/greet.txt');
 
 const writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
+const compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+
+const gzip = createGzip();
+
 readable.pipe(writable);
+
+// zlib library not saving it to a file
+// simply giving me an output
+readable.pipe(gzip).pipe(compressed);
